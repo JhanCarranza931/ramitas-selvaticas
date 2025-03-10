43d049167e97b7b01ad7df0726aa34e4ef864b70
Jhan Carranza <jhancarranza931@gmail.com>
2025-03-10 12:45:16 -0500

    ...

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <nav class="colo-red-600">
+            <a href="">Inicio</a>
+            <a href="">Inicio</a>
+            <a href="">Inicio</a>
+            <a href="">Inicio</a>
+        </nav>
+    </header>
+    <slot/>
+  </template>
+  
